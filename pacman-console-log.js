6456f589a124d1a615c5d2be7b35f2106a2c2447
donrestarone <shashikejayatunge@gmail.com>
2018-05-08 19:57:40 -0400

    half way done

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -8,3 +8,25 @@ console.log(ghosts[0]);
 console.log(ghosts[1]);
 console.log(ghosts[2]);
 console.log(ghosts[3]);
+
+console.log('objects are setup when they are first defined')
+console.log(ghosts);
+
+// ruby class equivalent in JS
+console.log('These are the attributes of the inky instance')
+var inky = {};
+inky.colour = 'Cyan';
+inky.personality = 'Shadow';
+console.log(inky);
+
+console.log('');
+
+// you can retrieve the values in your object by using . ir [] notation
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+// . notation
+console.log(ghosts.inky);
+console.log(ghosts.blinky);  
+
+// square bracket notation
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);
